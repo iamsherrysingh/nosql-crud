1f4a1795a46b5b9e93efca1357527b54bd711892
Sehaj <iamsherrysingh@gmail.com>
2026-03-01 01:23:54 -0800

    ADD Post Endpoint

--- src/main/java/ca/biglabs/nosqlcrud/dto/User.java
+++ src/main/java/ca/biglabs/nosqlcrud/dto/User.java
@@ -1,17 +1,30 @@
 package ca.biglabs.nosqlcrud.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Data
 @Document(collection = "users")
 public class User {
     @Id
     private String id;
+    @CreatedDate
+    private Date createdAt;
+    @NotBlank(message = "First name is required")
     private String firstName;
+    @NotBlank(message = "Last name is required")
     private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
     private String jobType;
+    @Min(value = 0, message = "Age must be 0 or greater")
     private Integer age;
 }
