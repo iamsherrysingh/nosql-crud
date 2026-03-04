6c9f9aa4e501131ee367d50889bc23a3dad0c44b
Sehajpreet Singh <iamsherrysingh@gmail.com>
2026-03-03 20:27:14 -0800

    Added Redis Cache (#7)
    * Implemented Redis Cache with Spring cache abstraction.
    
    * Added Redis Cache and mongo-express to docker-compose.yml

--- src/main/java/ca/biglabs/nosqlcrud/NosqlCrudApplication.java
+++ src/main/java/ca/biglabs/nosqlcrud/NosqlCrudApplication.java
@@ -2,10 +2,12 @@ package ca.biglabs.nosqlcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableCaching
 public class NosqlCrudApplication {
 
     public static void main(String[] args) {
